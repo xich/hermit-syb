d9080858ca2cf9dc724f3f0f5f6e005b80bef710
Jose Pedro Magalhaes <jpm@cs.ox.ac.uk>
2014-06-24 13:02:32 +0100

    Fewer repeated applies for this test

--- benchmarks/Hand/Map/Main.hs
+++ benchmarks/Hand/Map/Main.hs
@@ -4,5 +4,5 @@ import Auxiliary.Tree (smallerTree, sumTree, mapTree, Tree(..))
 import Auxiliary.Auxiliary (test, apply)
 
 mainTree :: IO ()
-mainTree = test (putStr (show (sumTree (apply 1000 (mapTree (+1)) smallerTree))))
+mainTree = test (putStr (show (sumTree (apply 100 (mapTree (+1)) smallerTree))))
 
--- benchmarks/SYBSpec/Map/Main.hs
+++ benchmarks/SYBSpec/Map/Main.hs
@@ -9,7 +9,7 @@ import Data.Generics
 import InlineGmap.Prelude
 
 mainTree :: IO ()
-mainTree = test (putStr (show (sumTree (apply 1000 (incTree (+1)) smallerTree))))
+mainTree = test (putStr (show (sumTree (apply 100 (incTree (+1)) smallerTree))))
 
 incTree :: (Int -> Int) -> (Tree Int -> Tree Int)
 incTree f = everywhere (mkT f)
