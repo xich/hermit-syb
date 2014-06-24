752f6b9f012f1511e25217703d5ccd151f136171
Jose Pedro Magalhaes <jpm@cs.ox.ac.uk>
2014-06-24 13:02:54 +0100

    Simplify the code

--- benchmarks/Tests.hs
+++ benchmarks/Tests.hs
@@ -9,13 +9,8 @@ import System.FilePath ((</>))
 --------------------------------------------------------------------------------
 
 data Library = Hand          -- Handwritten code
-             | HandPar       -- Handwritten code, parallelised
              | SYB           -- syb-0.1
-             | MultiRec      -- multirec-0.4
-             | MultiRecInline-- multirec-0.4 with INLINE pragmas
-             | Instant       -- instant-generics-0.1
-             | InstantInline -- instant-generics-0.1 with INLINE pragmas
-             | Deriving      -- generic-deriving-0.4 with INLINE pragmas
+             | SYBSpec       -- syb-0.1
                 deriving (Eq, Ord, Show)
 
 data TestName = Eq
@@ -50,6 +45,14 @@ htest l t d f = Test l t d hermitFlags
                                 ]
           modPrefix = "-fplugin-opt=HERMIT.Optimization.SYB:" ++ show l ++ "." ++ show t ++ ".Main:"
 
+
+stest :: Library -> TestName -> Datatype -> Test
+stest l t d = Test l t d flags
+  where flags = unwords ["-fplugin=InlineGmap"
+                        ,"-fplugin-opt=InlineGmap:$MODULE:\"*6 *6 *6 *6\""
+                        ,"-fexpose-all-unfoldings"
+                        ]
+
 data Datatype = Tree    -- Labelled binary trees
               | Logic   -- Logic expressions
               | Nat     -- Peano naturals
@@ -62,8 +65,7 @@ data Datatype = Tree    -- Labelled binary trees
 -- Tests
 --------------------------------------------------------------------------------
 
-allTests, handTests, sybTests, multirecTests, instantTests, instantInlineTests,
-  multirecInlineTests, derivingTests, tests :: [Test]
+allTests, handTests, sybTests, hermitSybTests, sybSpecTests :: [Test]
 handTests = [ test Hand Eq     Tree
             , test Hand Map    Tree
             , test Hand Read   Tree
@@ -82,21 +84,6 @@ handTests = [ test Hand Eq     Tree
             , test Hand SelectInt WTree
             , test Hand RenumberInt Tree
             , test Hand RenumberInt Logic]
-
-handParTests = [ test HandPar Eq     Tree
-               , test HandPar Map    Tree
-               --, test HandPar Read   Tree
-               , test HandPar Show   Tree
-               , test HandPar Update Tree
-               , test HandPar Enum   Tree
-               --, test HandPar Decode Tree
-               , test HandPar Eq     Logic
-               --, test HandPar Read   Logic
-               , test HandPar Show   Logic
-               , test HandPar Update Logic
-               , test HandPar Enum   Logic
-               --, test HandPar Decode Logic
-               ]
 
 sybTests = [ test SYB Eq     Tree
            , test SYB Map    Tree
@@ -125,71 +112,19 @@ hermitSybTests =
     , htest SYB RenumberInt Logic "mainLogic"
     ]
 
-multirecTests = [ test MultiRec Eq     Tree
-                , test MultiRec Show   Tree
-                , test MultiRec Read   Tree
-                , test MultiRec Update Tree
-                , test MultiRec Eq     Logic
-                , test MultiRec Show   Logic
-                -- , test MultiRec Read   Logic
-                , test MultiRec Update Logic]
-
-multirecInlineTests = [ test MultiRecInline Eq     Tree
-                      -- , test MultiRecInline Show   Tree
-                      -- , test MultiRecInline Read   Tree
-                      , test MultiRecInline Update Tree
-                      , test MultiRecInline Eq     Logic
-                      -- , test MultiRecInline Show   Logic
-                      -- , test MultiRecInline Read   Logic
-                      -- , test MultiRecInline Update Logic
-                      ]
-
-instantTests = [ test Instant Eq     Tree
-               , test Instant Show   Tree
-               , test Instant Update Tree
-               , test Instant Enum   Tree
-               , test Instant Enum   Nat
-               , test Instant Decode Tree
-               , test Instant Eq     Logic
-               , test Instant Show   Logic
-               , test Instant Update Logic
-               , test Instant Enum   Logic
-               , test Instant Decode Logic]
-
-instantInlineTests = [ test InstantInline Eq     Tree
-                     , test InstantInline Show   Tree
-                     , test InstantInline Update Tree
-                     , test InstantInline Enum   Tree
-                     , test InstantInline Enum   Nat
-                     , test InstantInline Decode Tree
-                     , test InstantInline Eq     Logic
-                     , test InstantInline Show   Logic
-                     , test InstantInline Update Logic
-                     , test InstantInline Enum   Logic
-                     , test InstantInline Decode Logic]
-
-derivingTests = [ test Deriving Eq     Tree
-                , test Deriving Map    Tree
-                , test Deriving Show   Tree
-                -- , test Deriving Update Tree
-                , test Deriving Enum   Tree
-                -- , test Deriving Decode Tree
-                , test Deriving Eq     Logic
-                , test Deriving Show   Logic
-                -- , test Deriving Update Logic
-                , test Deriving Enum   Logic
-                -- , test Deriving Decode Logic
-                , test Deriving Enum   Nat
-                ]
+sybSpecTests =
+    [ stest SYBSpec Map         Tree
+    , stest SYBSpec Update      Logic
+    , stest SYBSpec RmWeights   WTree
+    , stest SYBSpec SelectInt   WTree
+    , stest SYBSpec RenumberInt Tree
+    , stest SYBSpec RenumberInt Logic
+    ]
 
 
-allTests =    handTests ++ handParTests
-           ++ sybTests ++ hermitSybTests
-           ++ multirecTests ++ multirecInlineTests
-           ++ instantTests  ++ instantInlineTests
-           ++ derivingTests
+allTests = handTests ++ sybTests ++ hermitSybTests ++ sybSpecTests
 
 tests = [ t | t <- allTests
-        , lib t `elem` [Hand, SYB]
+        , lib t `elem` [Hand]
         , testName t `elem` [RenumberInt, Map, Update, RmWeights, SelectInt]
         ]
